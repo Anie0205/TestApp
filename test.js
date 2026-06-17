19b87cc1b9dc1285916fa782009c4ff26163907e
Ananya Verma <110318580+Anie0205@users.noreply.github.com>
2026-06-17 17:39:30 +0530

    Update test.js

--- test.js
+++ test.js
@@ -19,7 +19,7 @@ function (UWA, Promise, String, WAFData, PlatformAPI) {
 'use strict';
 
     var CONFIG = {
-        CSV_URL: widget.getValue('csvUrl') || './jnpa_vessel_timeseries.csv',
+        CSV_URL: widget.getValue('csvUrl') || 'jnpa_vessel_timeseries.csv',
         PLAYBACK_INTERVAL_MS: 3000,
         TRAIL_LENGTH: 10,
         MARKER_PREFIX: 'VESSEL_',
